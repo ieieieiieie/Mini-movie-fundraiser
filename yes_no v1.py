5633f5e3ad6ad0cc3bb77c3c59f7cd4cb9fd199d
William <131831056+ieieieiieie@users.noreply.github.com>
2024-03-20 09:24:01 +1300

    Add files via upload

---
+++ yes_no v1.py
@@ -0,0 +1,14 @@
+#functions
+
+
+#main routine
+instructions = input("would you like to read the instructions? ")
+
+if instructions == "yes":
+    print("print instructions")
+
+elif instructions == "no":
+    pass
+
+else:
+    print("invalid response, please answer yes or no")
